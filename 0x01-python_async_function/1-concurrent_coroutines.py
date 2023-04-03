051c9e4818f93da26b79c82f6ffc7363511fbb6e
abel mesfin <abelmesfin555@gmail.com>
2023-04-03 23:08:28 +0300

    pycodestyle requirement error fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""..."""
+"""execute multiple coroutines at the same time with async
+"""
+
 import asyncio
 from typing import List
 
@@ -8,12 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Create a list of tasks that call wait_random with the given max_delay"""
-
+    """Create a list of tasks that call wait_random with the given max_delay
+    """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-
-    # Wait for the tasks to complete and retrieve their results
     delays = [await task for task in asyncio.as_completed(tasks)]
-
-    # Return the list of delays in ascending order
     return delays
