46719aea5f1ac30959af2b289c6fa2f8e01b84d8
abel mesfin <abelmesfin555@gmail.com>
2023-04-03 23:56:20 +0300

    type error fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> List[float]::
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """..."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = [await task for task in asyncio.as_completed(tasks)]
