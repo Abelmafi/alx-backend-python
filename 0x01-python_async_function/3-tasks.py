2bab30f8329500d616a36f595a814016bb776846
abel mesfin <abelmesfin555@gmail.com>
2023-04-03 23:42:10 +0300

    inital commit

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Tasks'''
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay):
+    '''takes an integer max_delay and returns a asyncio.Task'''
+    tasks = asyncio.create_task(wait_random(max_delay))
+    return tasks
